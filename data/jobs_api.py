f447dc422a543bb4da606ab2bea70ba1a1629c2f
Stremyguy <stremyguy@gmail.com>
2025-03-22 18:17:31 +0300

    Added the creating job blueprint and the test.py file for testing correct and incorrect requests

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,8 +1,9 @@
 import flask
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 
 from . import db_session
 from .jobs import Jobs
+from .category import Category
 
 blueprint = flask.Blueprint(
     "jobs_api",
@@ -39,3 +40,30 @@ def get_one_job(job_id: int) -> dict:
                 "job", "leader.id", "work_size", "collaborators", "is_finished"))
         }
     )
+
+
+@blueprint.route("/api/jobs", methods=["POST"])
+def create_job() -> dict:
+    if not request.json:
+        return make_response(jsonify({"error": "Empty request"}), 400)
+    elif not all(key in request.json for key in
+                 ["job", "team_leader_id", "work_size", "collaborators", "categories", "is_finished"]):
+        return make_response(jsonify({"error": "Bad request"}), 400)
+    
+    session = db_session.create_session()
+
+    job = Jobs(
+        job=request.json["job"],
+        team_leader=request.json["team_leader_id"],
+        work_size=request.json["work_size"],
+        collaborators=request.json["collaborators"],
+        is_finished=request.json["is_finished"]
+    )
+    
+    categories = session.query(Category).filter(Category.id.in_(request.json["categories"])).all()
+    job.categories = categories
+    
+    session.add(job)
+    session.commit()
+    
+    return jsonify({"id": job.id})
--- test.py
+++ test.py
@@ -1,7 +1,29 @@
-from requests import get
+from requests import get, post
 
+# Некорректный запрос (пустой json, нет информации 
+# для добавления новой работы)
+print(post("http://127.0.0.1:5000/api/jobs",
+           json={}).json())
+
+# Некорректный запрос (не достаточно информации 
+# для добавления новой работы (только параметр "job"))
+print(post("http://127.0.0.1:5000/api/jobs",
+           json={"job": "Test job"}).json())
+
+# Некорректный запрос (пытаемся добавить 
+# несуществующий ключ "key")
+print(post("http://127.0.0.1:5000/api/jobs",
+           json={"key": 123}).json())
+
+# Корректный запрос
+print(post("http://127.0.0.1:5000/api/jobs",
+           json={"job": "Test job",
+                 "team_leader_id": 1,
+                 "work_size": 90,
+                 "collaborators": "1, 2, 3",
+                 "categories": [1],
+                 "is_finished": True}).json())
+
+# Убедимся, что созданная работа 
+# успешно добавилась
 print(get("http://127.0.0.1:5000/api/jobs").json())
-print(get("http://127.0.0.1:5000/api/jobs/1").json())
-
-print(get("http://127.0.0.1:5000/api/jobs/999").json())
-print(get("http://127.0.0.1:5000/api/jobs/abc").json())
