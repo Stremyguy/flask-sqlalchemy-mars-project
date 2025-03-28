2b1d134a87cbf5eab279d65793775f096424c16c
Pesyah <xxarturkrossxx@yandex.ru>
2025-03-28 18:20:04 +0300

    add postgresql

--- data/db_session.py
+++ data/db_session.py
@@ -5,28 +5,30 @@ from sqlalchemy.orm import Session
 SqlAlchemyBase = orm.declarative_base()
 
 __factory = None
+__engine = None
 
-
-def global_init(db_file: str) -> None:
+def global_init() -> None:
     global __factory
     
     if __factory:
         return
-    
-    if not db_file or not db_file.strip():
-        raise Exception("Необходимо указать файл базы данных.")
 
-    conn_str = f"sqlite:///{db_file.strip()}?check_same_thread=False"
+    conn_str = f"postgresql://postgres:postgres@127.0.0.1:5432/mars-egor"
     print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=False)
-    __factory = orm.sessionmaker(bind=engine)
+    global __engine
+    __engine = sa.create_engine(conn_str, echo=False)
+    __factory = orm.sessionmaker(bind=__engine)
 
     from . import __all_models
     
-    SqlAlchemyBase.metadata.create_all(engine)
+    SqlAlchemyBase.metadata.create_all(__engine)
 
 
 def create_session() -> Session:
     global __factory
     return __factory()
+
+def create_engine() -> Session:
+    global __engine
+    return __engine
